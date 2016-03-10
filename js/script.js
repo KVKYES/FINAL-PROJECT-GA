fe1fbfe40db8e4be94c42b5d9b38cf3d66750f73
KVKYES <winstontroysings@gmail.com>
2016-03-10 09:06:42 -0500

    Re-created index.html page to set the correct grid

--- js/script.js
+++ js/script.js
@@ -1,20 +1,51 @@
-// When the user clicks on the button,
-// togggle between hiding and showing the dropdown content
+var projectName;
 
-document.getElementById("myDropdown").classList.toggle("show");
-}
+$('.box').on('click', function(){
+	projectName = $(this).attr('data-project');
+	$('.'+projectName).show();
+});
 
-// Close the dropdown menu if the user clicks outside of it
+$('.box .close-btn').on('click', function(){
+	$(this).parent().hide();
+});
 
-window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
 
-		var dropdowns = document.getElementByClassName ("dropdown-content");
-		for (var d = 0; d < dropdowns.length; d++) {
-			var openDropdown = dropdowns[d];
-			if (openDropdown.classList.contains('show')) {
-				openDropdown.classList.remove('show');
-			}
-		}
-	}
-}
+
+
+// // // When the user clicks on the button,
+// // // togggle between hiding and showing the dropdown content
+
+// // function myFunction() {
+// // 	document.getElementById("myDropdown").classList.toggle("show");
+// // }
+
+// // // Close the dropdown menu if the user clicks outside of it
+
+// // window.onclick = function(event) {
+// // 	if (!event.target.matches('.dropbtn')) {
+
+// // 		var dropdowns = document.getElementByClassName ("dropdown-content");
+// // 		var = i;
+// // 		for (i = 0; i < dropdowns.length; d++) {
+// // 			var openDropdown = dropdowns[i];
+// // 			if (openDropdown.classList.contains('show')) {
+// // 				openDropdown.classList.remove('show');
+// // 			}
+// // 		}
+// // 	}
+// // }
+
+
+
+
+// Just saving this here in case i need jquery for dropdown /////
+
+
+// // 	<!-- 			<div class="dropdown">
+// // 					<button onclick="myFunction()" class="dropbtn">Design</button>
+// // 					<div id="myDropdown" class="dropdown-content">
+// //     					<a href="design-cd-lp.html">CD/LP Design</a>
+// //     					<a href="posters.html">Posters</a>
+// //     					<a href="identity.html">Identity</a>
+// //   </div>
+// // </div> -->
